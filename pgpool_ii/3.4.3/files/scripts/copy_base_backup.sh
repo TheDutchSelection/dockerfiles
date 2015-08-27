a31a1cfed0ce22d4ef59533ca5640b1210b453e3
Gerard Meijer <gerard@meijer.gs>
2015-08-27 14:11:25 +0200

    More debugging and added nano for easier debugging inside container.

--- pgpool_ii/3.4.3/files/scripts/copy_base_backup.sh
+++ pgpool_ii/3.4.3/files/scripts/copy_base_backup.sh
@@ -5,8 +5,9 @@ data_directory=$1
 recovery_target=$2
 recovery_data_directory="/tmp"
 
+echo "psql select pgpool-recovery..."
 /usr/bin/psql -c "select pg_start_backup('pgpool-recovery')" postgres
-echo "restore_command = '/usr/bin/ssh -T ""$HOST_IP"" \'mkdir -p /tmp/archive_log;docker run --volumes-from postgresql_master_data --rm -v /tmp/archive_log:/tmp/archive_log --entrypoint=\"cp ""$data_directory""archive_log/*" "/tmp/archive_log/\" thedutchselection/data:latest\';/usr/bin/scp ""$HOST_IP"":""$data_directory""archive_log/%f %p" > "$data_directory""recovery.conf"
-/bin/tar -C "$data_directory" -zcf pgsql.tar.gz "$data_directory"
-/usr/bin/psql -c 'select pg_stop_backup()' postgres
-/usr/bin/scp pgsql.tar.gz $recovery_target:$recovery_data_directory
+#echo "restore_command = '/usr/bin/ssh -T ""$HOST_IP"" \'mkdir -p /tmp/archive_log;docker run --volumes-from postgresql_master_data --rm -v /tmp/archive_log:/tmp/archive_log --entrypoint=\"cp ""$data_directory""archive_log/*" "/tmp/archive_log/\" thedutchselection/data:latest\';/usr/bin/scp ""$HOST_IP"":""$data_directory""archive_log/%f %p" > "$data_directory""recovery.conf"
+#/bin/tar -C "$data_directory" -zcf pgsql.tar.gz "$data_directory"
+#/usr/bin/psql -c 'select pg_stop_backup()' postgres
+#/usr/bin/scp pgsql.tar.gz $recovery_target:$recovery_data_directory

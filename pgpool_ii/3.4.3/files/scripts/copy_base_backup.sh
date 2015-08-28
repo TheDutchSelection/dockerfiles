28b8d9840756c92445b96c5ca7af280135f91699
Gerard Meijer <gerard@meijer.gs>
2015-08-28 20:42:41 +0200

    Fixes.

--- pgpool_ii/3.4.3/files/scripts/copy_base_backup.sh
+++ pgpool_ii/3.4.3/files/scripts/copy_base_backup.sh
@@ -7,7 +7,7 @@ recovery_target=$2
 echo "pg_start_backup..."
 /usr/bin/psql -c "select pg_start_backup('pgpool-recovery')" postgres
 echo "restore_command = 'wal-e wal-fetch %f %p'" > "$data_directory""recovery.conf"
-if [[ -z "$AWS_S3_WALE_BUCKET_BASE_PATH" || "$AWS_S3_WALE_BUCKET_BASE_PATH" =="/" ]]; then
+if [[ -z "$AWS_S3_WALE_BUCKET_BASE_PATH" || "$AWS_S3_WALE_BUCKET_BASE_PATH" == "/" ]]; then
   wal-e --s3-prefix="s3://""$AWS_S3_WALE_BUCKET_NAME""/""$HOST_IP" backup-push "$data_directory"
 else
   wal-e --s3-prefix="s3://""$AWS_S3_WALE_BUCKET_NAME""/""$AWS_S3_WALE_BUCKET_BASE_PATH""$HOST_IP" backup-push "$data_directory"
--- postgresql/9.4.4/files/scripts/run.sh
+++ postgresql/9.4.4/files/scripts/run.sh
@@ -61,7 +61,7 @@ create_postgresql_conf () {
     # set environment variables matching WAL-E preferences
     AWS_ACCESS_KEY_ID="$AWS_S3_WALE_ACCESS_KEY_ID"
     AWS_SECRET_ACCESS_KEY="$AWS_S3_WALE_SECRET_ACCESS_KEY"
-    if [[ -z "$AWS_S3_WALE_BUCKET_BASE_PATH" || "$AWS_S3_WALE_BUCKET_BASE_PATH" =="/" ]]; then
+    if [[ -z "$AWS_S3_WALE_BUCKET_BASE_PATH" || "$AWS_S3_WALE_BUCKET_BASE_PATH" == "/" ]]; then
       WALE_S3_PREFIX="\"s3://""$AWS_S3_WALE_BUCKET_NAME""/""$HOST_IP""\""
     else
       WALE_S3_PREFIX="\"s3://""$AWS_S3_WALE_BUCKET_NAME""/""$AWS_S3_WALE_BUCKET_BASE_PATH""$HOST_IP""\""

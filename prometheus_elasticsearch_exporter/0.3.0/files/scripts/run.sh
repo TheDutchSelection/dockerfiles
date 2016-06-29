3197f0077ca875bdae741a237991f85d2072e9c7
Gerard Meijer <gerard@meijer.gs>
2016-06-29 16:17:16 +0200

    Fixed prometheus elasticsearch exporter.

--- prometheus_elasticsearch_exporter/0.3.0/files/scripts/run.sh
+++ prometheus_elasticsearch_exporter/0.3.0/files/scripts/run.sh
@@ -2,4 +2,4 @@
 set -e
 
 echo "starting prometheus node exporter..."
-exec /usr/local/bin/elasticsearch_exporter -es.all=false -es.uri="$ELASTICSEARCH_URI" -web.listen-address=9108
+exec /usr/local/bin/elasticsearch_exporter -es.all=false -es.uri="$ELASTICSEARCH_URI" -web.listen-address=":9108"

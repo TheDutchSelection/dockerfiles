9ff1398507afbeb9cd8290523484310d1cfb09c2
Gerard Meijer <gerard@meijer.gs>
2016-06-29 12:34:53 +0200

    Elasticsearch prometheus stats exporter.

---
+++ prometheus_elasticsearch_exporter/0.3.0/files/scripts/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+echo "starting prometheus node exporter..."
+exec /usr/local/bin/elasticsearch_exporter -es.all=false -es.uri="$ELASTICSEARCH_URI" -web.listen-address="$WEB_LISTEN_PORT"

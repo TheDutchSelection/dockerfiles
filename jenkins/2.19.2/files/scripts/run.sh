2ba08a5c2fcae8cb65cceac8f42fafb9b19e0eb8
Gerard Meijer <gerard@meijer.gs>
2016-11-07 12:08:32 +0100

    Readded ssh

--- jenkins/2.19.2/files/scripts/run.sh
+++ jenkins/2.19.2/files/scripts/run.sh
@@ -4,6 +4,13 @@ set -e
 # remove any existing docker pid
 rm -f /var/run/docker.pid
 
+if [[ -f "$SSH_KEY" && -f  "$SSH_PUB_KEY" ]]; then
+  chown jenkins:jenkins "$SSH_KEY"
+  chown jenkins:jenkins "$SSH_PUB_KEY"
+  chmod 400 "$SSH_KEY"
+  chmod 400 "$SSH_PUB_KEY"
+fi
+
 echo "set JENKINS_HOME to $DATA_DIRECTORY"
 export JENKINS_HOME="$DATA_DIRECTORY"
 

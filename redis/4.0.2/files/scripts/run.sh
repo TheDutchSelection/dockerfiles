f183331e385297a1d1302cf8d9c4aaacc9cc0b8f
Gerard Meijer <gerard@meijer.gs>
2017-11-24 08:29:22 +0100

    New redis

--- redis/4.0.2/files/scripts/run.sh
+++ redis/4.0.2/files/scripts/run.sh
@@ -11,6 +11,9 @@ sed -i "s/##data_directory##/$escaped_data_directory/g" /etc/redis/redis.conf
 if [[ ! -z "$PASSWORD" ]]; then
   sed -i "s/##password##/requirepass $PASSWORD/g" /etc/redis/redis.conf
 fi
+if [[ ! -z "$MAX_MEMORY_IN_BYTES" ]]; then
+  sed -i "s/##max_memory##/maxmemory $MAX_MEMORY_IN_BYTES/g" /etc/redis/redis.conf
+fi
 
 echo "starting redis..."
 exec /usr/local/bin/redis-server /etc/redis/redis.conf

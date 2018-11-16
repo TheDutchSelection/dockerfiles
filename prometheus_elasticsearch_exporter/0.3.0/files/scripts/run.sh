5f53c4fdca1342d566dd87546c878ef26ab87d55
Gerard Meijer <gerard@meijer.gs>
2018-11-16 13:48:26 +0100

    Prometheus server 2.5.0 and Prometheus Alertmanager 0.15.3

--- prometheus_elasticsearch_exporter/0.3.0/files/scripts/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "starting prometheus elasticsearch exporter..."
-exec /usr/local/bin/elasticsearch_exporter -es.all=false -es.uri="$ELASTICSEARCH_URI" -web.listen-address=":9108"

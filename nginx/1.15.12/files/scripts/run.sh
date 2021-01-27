c3cdfdad7e5292616fc0f793af58173b8dcbbb56
Gerard Meijer <gerard@meijer.gs>
2021-01-27 11:43:20 +0100

    Escape some $

--- nginx/1.15.12/files/scripts/run.sh
+++ nginx/1.15.12/files/scripts/run.sh
@@ -53,9 +53,9 @@ read -r -d '' reverse_proxy_server_base << EOM || true
     location ^~ /cable {
       proxy_pass http://##app##;
       proxy_http_version 1.1;
-      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-      proxy_set_header Host $http_host;
-      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+      proxy_set_header Host \$http_host;
+      proxy_set_header Upgrade \$http_upgrade;
       proxy_set_header Connection "Upgrade";
     }
 

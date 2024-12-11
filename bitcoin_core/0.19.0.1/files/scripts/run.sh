8c4c25610d896533d9b52c8c66ec3d2a1f81f7a3
Gerard Meijer <gerard@meijer.gs>
2024-12-11 11:04:23 +0100

    Removed no longer needed images

--- bitcoin_core/0.19.0.1/files/scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "creating persistant directory..."
-mkdir -p "$DATA_PATH"
-
-echo "starting bitcoin core..."
-exec bitcoind -disablewallet -datadir="$DATA_PATH"
--- zyte_headless_proxy/1.3.2/files/scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "starting zyte headless proxy..."
-/usr/local/bin/crawlera-headless-proxy -d -c /etc/zhp/config.toml -a "$ZYTE_SPM_API_KEY"
-
-# wait for the pid of this file to end
-wait $!
